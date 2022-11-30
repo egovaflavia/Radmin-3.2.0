dc7dc0a6216273deec9a79bce12fb077515d8df8
Egova Riva Gustino <50259464+egovaflavia@users.noreply.github.com>
2022-12-01 00:27:06 +0700

    Update
    Komentar

--- public/plugins/DataTables/datatables.js
+++ public/plugins/DataTables/datatables.js
@@ -75038,7 +75038,8 @@ $.extend( DataTable.ext.classes, {
 	sWrapper:      "dataTables_wrapper dt-bootstrap4",
 	sFilterInput:  "form-control form-control-sm",
 	sLengthSelect: "custom-select custom-select-sm form-control form-control-sm",
-	sProcessing:   "dataTables_processing ",
+	sProcessing:   "dataTables_processing",
+    // Egova 1 12 22 Ubah Spin Datatable
 	sPageButton:   "paginate_button page-item"
 } );
 
